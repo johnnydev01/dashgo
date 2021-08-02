3f8053f9b2b08210f34ccc29487437cf16c93034
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-02 19:50:24 -0300

    configurando fonte

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,10 @@ export const theme = extendTheme({
             "50": "#EEEEF2",
           }
     },
+    fonts: {
+        heanding: 'Roboto',
+        body: 'Roboto'
+    },
     styles: {
         global: {
             body: {
