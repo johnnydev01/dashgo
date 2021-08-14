71946cb22c1add464c85af90edbecbdf0829df5f
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-14 12:45:05 -0300

    Factories e seeds

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Flex, Icon, Heading, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -11,6 +11,12 @@ export default function UserList() {
         base: false,
         lg: true,
     });
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, []);
 
     return (
         <Box>
