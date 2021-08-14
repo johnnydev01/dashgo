cb44055c84a7c435da74eaed54cf3524fd6ea1cb
Johnny Ferreira da Silva <joferres@everis.com>
2021-08-14 13:36:22 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,9 +11,21 @@ import { useQuery } from 'react-query';
 export default function UserList() {
     const { data, isLoading, error} = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
-    const data = response.json();
+    const data = await response.json();
 
-    return data;
+    const users =  data.users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            }),
+        }
+    })
+    return users;
 });
 
     
@@ -75,31 +87,35 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4","4","6"]}>
-                                            <Checkbox colorScheme="pink"/>
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Johnny Silva</Text>
-                                                <Text fontSize="sm" color="gray.300">johnnysilva.dev@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        { isWideVersion && <Td>04 de Abril. 2021</Td>}
-                                        <Td >
-                                            <Link href="/users/edit" passHref>  
-                                                <Button
-                                                    as="a"
-                                                    size="sm"
-                                                    fontSize="sm"
-                                                    colorScheme="purple"
-                                                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                                >
-                                                    { isWideVersion ? 'Editar': ''}
-                                                </Button>
-                                            </Link>
-                                        </Td>
-                                    </Tr>
+                                   {data.map(user => {
+                                       return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4","4","6"]}>
+                                                    <Checkbox colorScheme="pink"/>
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && <Td>{user.createdAt}</Td>}
+                                                <Td>
+                                                    <Link href="/users/edit" passHref>  
+                                                        <Button
+                                                            as="a"
+                                                            size="sm"
+                                                            fontSize="sm"
+                                                            colorScheme="purple"
+                                                            leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                                                        >
+                                                            { isWideVersion ? 'Editar': ''}
+                                                        </Button>
+                                                    </Link>
+                                                </Td>
+                                            </Tr>
+                                       )
+                                   })}
                                 </Tbody>
                             </Table>
                             <Pagination />
